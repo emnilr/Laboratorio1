fedd3cedee528e7102ccac2a83d878dc136ebb1d
emnilr <emil3991.hern9andez@gmail.com>
2025-04-23 10:16:15 -0500

    Archivo principal

---
+++ calculadora.py
@@ -0,0 +1,49 @@
+def suma():
+
+def resta():
+
+def multiplicacion():
+
+def division():
+
+def logaritmo():
+
+def potencia():
+
+
+
+
+
+
+
+
+
+
+def main():
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicacion")
+    print("4. Division")
+    print("5. Logaritmo")
+    print("6. Potencia ")
+
+    opcion = input("Ingrese el numero para la función que desea realizar:")
+
+    match opcion:
+        case "1":
+            suma()
+        case "2":
+            resta()
+        case "3":
+            multiplicacion()
+        case "4":
+            division()
+        case "5":
+            logaritmo()
+        case "6":
+            potencia()
+
+
+
+
+main()
