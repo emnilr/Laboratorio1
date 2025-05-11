ba10b45aa5760e3498dee6c8852fd5f1fa315e2e
Alejor1110 <davidalejandrogaitanhernandez@gmail.com>
2025-05-11 18:17:16 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,30 @@
-def suma():
+def suma(): 
+    a = int(input("Digite numero: "))
+    b = int(input("Digite numero: "))
 
+    c = a + b
+    print(c)
+    
 def resta():
+    a =  int(input("Digite numero: "))
+    b =  int(input("Digite numero: "))
 
+    c = a - b
+    print(c)
+    
 def multiplicacion():
+    a =  int(input("Digite numero: "))
+    b =  int(input("Digite numero: "))
 
+    c = a * b
+    print (C)
+    
 def division():
+   a = int(input("Digite numero: "))
+   b = int(input("Digite numero: "))
 
+   c = a / b
+   print(c)
 def logaritmo():
 
 def potencia():
